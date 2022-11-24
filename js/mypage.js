368c8755d672645c189714e870b8bf55d34b2dc2
sieun <ja24z8@naver.com>
2022-11-24 22:15:27 +0900

    [수정] 22.11.24 마이페이지 자스로 메세지출력추가,11/23정기회의내용으로 수정

---
+++ js/mypage.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    const name = document.querySelector("#user-name")
+    const email = document.querySelector("#user-email")
+    const tel = document.querySelector("#user-tel")
+    const connect = document.querySelector("#user-lastconnect")
+    name.innerHTML = "배고픈 딸기"+"님"
+    email.innerHTML = "something@some.com"
+    tel.innerHTML = "010 . 1234 . 1234"
+    connect.innerHTML = "2022.11.06 17:4"
+}
