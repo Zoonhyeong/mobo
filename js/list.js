9fa85f9dba486b9593e203c596d6291679130424
이준형 <wkcpq35@naver.com>
2022-11-27 23:38:38 +0900

    [추가, 수정] 22.11.27 <subscribe, list 수정, subscribe_create 추가>

--- js/list.js
+++ js/list.js
@@ -2,10 +2,10 @@ function drop_sub_list1(){
     let click = document.getElementById("drop-content");
     if(click.style.display === "none"){
         click.style.display = "block";
-
+        $("#drop-content").stop().slideToggle(-1000).slideDown(700);
     }else{
         click.style.display = "none";
-
+        $("#drop-content").stop().slideToggle(-1000).slideUp(700);
     }
 }
 
@@ -13,9 +13,25 @@ function drop_sub_list2(){
     let click = document.getElementById("drop-content2");
     if(click.style.display === "none"){
         click.style.display = "block";
-
+        $("#drop-content2").stop().slideToggle(-1000).slideDown(700);
     }else{
         click.style.display = "none";
-
+        $("#drop-content2").stop().slideToggle(-1000).slideUp(700);
     }
+}
+
+function lastPage(){
+    history.back();
+}
+
+function nextPage(){
+    location.href="subscribe_create.html"
+}
+
+function subDetail(){
+    location.href="subscribe.html"
+}
+
+function listPage(){
+    location.href="list.html"
 }
