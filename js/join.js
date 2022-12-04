304b44ee38de80d2ca2cc8d857f63eefa40f0e0a
sieun <ja24z8@naver.com>
2022-12-05 02:33:15 +0900

    [추가] 22/12/05 비번 틀릴시 빨간 선 보임

--- js/join.js
+++ js/join.js
@@ -1,8 +1,11 @@
-function checkPw(){
-    const pw = document.querySelector("#user-pw");
-    const pw_check = document.querySelector("#user-pw-check");
-    const message = document.querySelector("#check-massage")
-    if(pw.value !== pw_check.value){
-        
-}
-}
+document.body.addEventListener('keyup',()=>{
+    const pw = document.getElementById("user-pw")
+    const pw_check = document.getElementById("user-pw-check")
+    const red_bar = document.getElementById("checking-red-bar")
+    
+    if(pw.value === pw_check.value){
+        red_bar.style.display = 'none'
+    } else{
+        red_bar.style.display = 'block'
+    }
+})
